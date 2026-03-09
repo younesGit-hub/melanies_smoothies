9a7d2a46d64efac3f9d61cd79055d99576f2b48f
younesGit-hub <bentatayounes@hotmail.com>
2026-03-09 15:41:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,7 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
+#New section
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
