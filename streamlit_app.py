58b4f2b6088fbd2bf1d999365337bb7b5b2af6cd
younesGit-hub <bentatayounes@hotmail.com>
2026-03-10 07:46:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,5 +52,5 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + 'Nutrition Information')
       # Appeler l'API (exemple avec watermelon)
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + fruit_chosen )
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon" + col('search_on') )
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
