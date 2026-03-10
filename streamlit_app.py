14b381b12cf3ec38a3d79014e60ce333ed1edbf1
younesGit-hub <bentatayounes@hotmail.com>
2026-03-10 08:02:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,10 @@ if ingredients_list:
     # Construire la chaîne des fruits choisis
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+      
         st.subheader(fruit_chosen + 'Nutrition Information')
       # Appeler l'API (exemple avec watermelon)
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"  )
